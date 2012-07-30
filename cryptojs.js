9661ad150271879cd21e1128c3039bf48fe25c25
Jack Xiang Cheng <jackxiangcheng@Jack-Ubuntu-New.(none)>
2012-07-30 19:58:37 +1000

    second update

---
+++ cryptojs.js
@@ -0,0 +1,8 @@
+var CryptoJS = require('./lib/core').CryptoJS;
+require('./lib/enc-base64');
+require('./lib/md5');
+require('./lib/evpkdf');
+require('./lib/cipher-core');
+require('./lib/aes');
+
+//exports.CryptoJS = CryptoJS;
